19be4fb4e283be91a2c176a493dd56d5274e31df
Juptian <juptianraffles@gmail.com>
2020-10-14 21:03:03 -0400

    Create no.cpp

---
+++ no.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+#define no ::
+#define noo true
+#define nooo using
+#define noooo namespace
+#define nooooo std
+#define noooooo int
+#define nooooooo main
+#define noooooooo cout
+#define nooooooooo cin
+#define noooooooooo get
+#define nooooooooooo ;
+#define noooooooooooo (
+#define nooooooooooooo )
+#define noooooooooooooo {
+#define nooooooooooooooo }
+#define noooooooooooooooo <<
+
+nooo noooo nooooo nooooooooooo
+
+    noooooo nooooooo noooooooooooo nooooooooooooo
+        noooooooooooooo
+            nooooo no noooooooo noooooooooooooooo "Test 1\n" nooooooooooo
+                nooooo no noooooooo noooooooooooooooo "\nTest 2\n" nooooooooooo
+                    nooooo no noooooooo noooooooooooooooo "\nTest 3\n" nooooooooooo
+                        nooooo no nooooooooo.noooooooooo noooooooooooo nooooooooooooo nooooooooooo
+                            nooooooooooooooo
